536d4d96827b3a0b8058df3a5d116baea3bbe005
melohan <melodie.ohan@cpnv.ch>
2021-12-02 08:11:56 +0100

    Add getLastUpdated() method to Practice

--- app/Models/Practice.php
+++ app/Models/Practice.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Practice extends Model
 {
     use HasFactory;
+
+    public static function getLastUpdated(int $n = 5)
+    {
+        return self::whereDate('updated_at', '<=', Carbon::now()->subDays($n))->get();
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,22 @@
 @extends('layout')
-<h1>Welcome</h1>
+@section('content')
+    <h1 class="text-xl" xmlns="http://www.w3.org/1999/html">Welcome</h1>
+    <table class="min-w-full divide-y divide-gray-200">
+        <thead class="bg-gray-50">
+        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Date
+        </th>
+        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Description
+        </th>
+        </thead>
+        <tbody class="bg-white divide-y divide-gray-200">
+        @foreach($data as $practice)
+            <tr>
+                <td class="px-6 py-4 whitespace-nowrap">{{date('Month D, Yr', strtotime($practice['created_at']))}}</td>
+                <td>{{$practice['description']}}</td>
+            </tr>
+        @endforeach
+        </thead>
+    </table>
+@stop
