98cbb458c826cc7f1fcfbc408496e429b89f9f34
melohan <melodie.ohan@cpnv.ch>
2021-12-10 10:06:26 +0100

    Update packages and css path

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
     <title>Priki</title>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link href="css/tailwind/tailwind.css" rel="stylesheet">
+    <link href="{{asset("css/app.css") }}" rel="stylesheet">
     <style>
         @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
         .font-family-karla {
