5776b5e111e4f2ee8262f3eccf27c0edb5204e1e
melohan <melodie.ohan@cpnv.ch>
2021-12-10 10:21:17 +0100

    Fix livewire error at home page

--- app/Http/Livewire/Practice/LastUpdates.php
+++ app/Http/Livewire/Practice/LastUpdates.php
@@ -7,7 +7,6 @@ use App\Models\Practice;
 
 class LastUpdates extends Component
 {
-    public $practices = [];
     public $days = 5;
 
     public function increment()
@@ -19,7 +18,7 @@ class LastUpdates extends Component
     {
         // On repasse à 5 si on injecte une valeur éronnée.
         $this->days = intval($this->days);
-        $this->practices =  Practice::getLastUpdated($this->days);
-        return view('livewire.practice.last-updates');
+        $practices =  Practice::getLastUpdated($this->days);
+        return view('livewire.practice.last-updates', ['practices'=>$practices]);
     }
 }
--- resources/views/livewire/practice/last-updates.blade.php
+++ resources/views/livewire/practice/last-updates.blade.php
@@ -1,40 +1,41 @@
-Nouveau de {{$days}} jours.
-<br>
-<div class="mb-4">
-    <label class="block text-gray-700 text-sm font-bold mb-2">
-        Afficher les derniers
-    </label>
-    <input
-        wire:model="days"
-        id="jours"
-        type="text"
-        placeholder="days"
-        class="shadow appearance-none border rounded w-full py-2 px-1 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-    >
-    <label class="block text-gray-700 text-sm font-bold mb-2">
-        jours
-    </label>
-</div>
+<div>
+    Nouveau de {{$days}} jours.
+    <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2">
+            Afficher les derniers
+        </label>
+        <input
+            wire:model="days"
+            id="jours"
+            type="number"
+            placeholder="days"
+            class="shadow appearance-none border rounded w-full py-2 px-1 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        >
+        <label class="block text-gray-700 text-sm font-bold mb-2">
+            jours
+        </label>
+    </div>
 
-<br>
-@empty($practices)
-    <p>Aucune pratique à afficher ici.</p>
-@else
-    <table class="min-w-full divide-y divide-gray-200">
-        <thead class="bg-gray-50 text-center">
-        <th scope="col" class="px-6 py-3 text-xs font-bold text-gray-500 uppercase tracking-wider">
-            Date
-        </th>
-        <th scope="col" class="px-6 py-3 text-xs font-bold text-gray-500 uppercase tracking-wider">
-            Description
-        </th>
-        </thead>
-        <tbody class="bg-white divide-y divide-gray-200">
-        @foreach($practices as $practice)
-            <td class="px-6 py-4 whitespace-nowrap">{{\Carbon\Carbon::parse($practice->created_at)->formatLocalized('%d %B %Y')}}</td>
-            <td>{{$practice->description}}</td>
-            </tr>
-        @endforeach
-        </thead>
-    </table>
-@endif
+    <br>
+    @empty($practices)
+        <p>Aucune pratique à afficher ici.</p>
+    @else
+        <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50 text-center">
+            <th scope="col" class="px-6 py-3 text-xs font-bold text-gray-500 uppercase tracking-wider">
+                Date
+            </th>
+            <th scope="col" class="px-6 py-3 text-xs font-bold text-gray-500 uppercase tracking-wider">
+                Description
+            </th>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+            @foreach($practices as $practice)
+                <td class="px-6 py-4 whitespace-nowrap">{{\Carbon\Carbon::parse($practice->created_at)->formatLocalized('%d %B %Y')}}</td>
+                <td>{{$practice->description}}</td>
+                </tr>
+            @endforeach
+            </thead>
+        </table>
+    @endif
+</div>
