b169cd49ae243eacd116789c46c189cbf0d6a4cd
melohan <melodie.ohan@cpnv.ch>
2021-12-14 11:56:00 +0100

    Set domain in session

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Domain;
 use Illuminate\Http\Request;
 use App\Models\Practice;
 use Illuminate\Support\Str;
+use MongoDB\Driver\Session;
 
 class PracticeController extends Controller
 {
     public function domain(int $id = null)
     {
+        $domain = Domain::find($id);
+        \session(['domain'=>$domain]);
         $practices = isset($id) ? Practice::getPublishedPerDomain($id) : Practice::getPublished();
         return view('practice-per-domain', ['practices' => $practices, 'id' => $id]);
     }
