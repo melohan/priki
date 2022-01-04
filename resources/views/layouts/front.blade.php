e6254ce84dc950816e2db059c5d4265e33dc5a0f
melohan <melodie.ohan@cpnv.ch>
2022-01-05 00:04:09 +0100

    Reorganize head script and style

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -1,21 +1,22 @@
 <!doctype html>
 <html>
 <head>
-    <title>Priki</title>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link href="{{asset("css/app.css") }}" rel="stylesheet">
-    <style>
-        @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
-        .font-family-karla {
-            font-family: karla;
-        }
-    </style>
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @livewireStyles
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"
             integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
-    @livewireStyles
+
 </head>
 <body class="bg-white font-family-karla">
 
