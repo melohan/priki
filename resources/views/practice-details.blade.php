1e88048e8ea912ebc494d7e546abc2ab18b67108
melohan <melodie.ohan@cpnv.ch>
2022-01-05 02:05:39 +0100

    Add users opinions

--- resources/views/practice-details.blade.php
+++ resources/views/practice-details.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 
     @if(!is_null($practice))
+        <!-- Practice -->
         <article class="w-4/5 shadow rounded-md text-gray-800 mb-10">
             <div class="h-10 bg-purple-200 rounded-t-md p-2 flex space-x-80 px-4 ">
                 <div class="w-4/6"><h1 class="font-bold text-lg ">
@@ -33,6 +34,46 @@
                 <p class="text-justify">{{$practice->description}}</p>
             </div>
         </article>
+        <!-- Opinion -->
+
+        @if(!is_null($practice->opinions))
+            <article class="w-4/5 shadow rounded-md text-gray-800 mb-10">
+                <div class="h-10 bg-gray-100 rounded-t-md p-2 flex space-x-80 px-4 ">
+                    <div class="w-4/6"><h1 class="font-bold text-lg ">
+                            Opinions
+                        </h1>
+                    </div>
+                </div>
+                @foreach($practice->opinions as $opinion)
+                    <div class="rounded-b-md bg-gray-50 p-4 divide-y">
+                        <table class="table-auto mb-5">
+                            <tr>
+                                @if(!is_null($practice->domain->user))
+                                    <td class="p-1">{{$practice->domain->user()->name}}</td>
+                                @endif
+                            </tr>
+                            <tr>
+                                <td class="font-bold p-1">Auteur</td>
+                                <td class="p-1">{{$practice->user->fullname}}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-bold p-1">Date de création</td>
+                                <td class="p-1">{{\Carbon\Carbon::parse($opinion->created_at)->formatLocalized('%d %B %Y')}}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-bold p-1">Date de modification</td>
+                                <td class="p-1">{{\Carbon\Carbon::parse($opinion->updated_at)->formatLocalized('%d %B %Y')}}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-bold p-1">Description</td>
+                                <td><p class="text-justify">{{$opinion->description}}</p></td>
+                            </tr>
+                        </table>
+
+                    </div>
+                @endforeach
+            </article>
+        @endif
     @else
         <div class="w-5/6 bg-purple-100 border-l-4 border-purple-400 text-orange-700 p-4" role="alert">
             <p class="font-bold text-center">
