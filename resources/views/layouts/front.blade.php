92588baa087d08eb6de3675c4c4eaefd4ad6faf6
melohan <melodie.ohan@cpnv.ch>
2022-01-17 22:08:58 +0100

    Add Alpin js framework

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -16,6 +16,8 @@
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"
             integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
+    <!-- Alpin js -->
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
 
 </head>
 <body class="bg-white font-family-karla">
@@ -42,6 +44,7 @@
         @yield('content')
     </div>
 </div>
+
 
 <script src="https://unpkg.com/@themesberg/flowbite@latest/dist/flowbite.bundle.js"></script>
 @livewireScripts
