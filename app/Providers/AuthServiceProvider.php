4a61499d2e8880912d2d59ed6867bde42cabb969
melohan <melodie.ohan@cpnv.ch>
2022-01-18 12:05:15 +0100

    Define moderator policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('moderator', function (User $user) {
+            return $user->slug === 'MOD';
+        });
     }
 }
