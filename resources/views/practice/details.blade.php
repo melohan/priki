6771c23624b66ec6a83dc4fd9271cb13cf36e730
melohan <melodie.ohan@cpnv.ch>
2022-01-20 10:27:19 +0100

    Set text color white

--- resources/views/practice/details.blade.php
+++ resources/views/practice/details.blade.php
@@ -9,8 +9,8 @@
             <x-opinion.opinion :practice="$practice"/>
         @endif
     @else
-        <div class="w-5/6 bg-gray-700 border-l-4 border-gray-400 p-4" role="alert">
-            <p class="font-bold text-center">
+        <div class="w-5/6 bg-gray-700 border-l-4 border-gray-400 text-white p-4" role="alert">
+        <p class="font-bold text-center">
                 Aucune pratique à afficher ici.
             </p>
         </div>
