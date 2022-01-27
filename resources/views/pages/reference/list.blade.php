92593afe981197cfa0fa0a16df96dfc5531487a6
melohan <melodie.ohan@cpnv.ch>
2022-01-27 11:07:56 +0100

    Add create page and add button to the reference list page

--- resources/views/pages/reference/list.blade.php
+++ resources/views/pages/reference/list.blade.php
@@ -1,5 +1,13 @@
 <x-app-layout>
-    <h1>Liste des références</h1>
+    <h1 class="text-xl	">Liste des références</h1>
+    <br>
+    <a class="2xl:font-bold mx-8" href="{{route('references.create')}}">
+        <button
+            class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 inline-flex items-center">
+            <span>Ajouter une référence</span>
+        </button>
+    </a>
+    <br>
     <div class="w-5/6">
         <div class="grid grid-cols-3 gap-4 ">
             @foreach($references as $reference)
