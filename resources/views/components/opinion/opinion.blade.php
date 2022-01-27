4bbc0bb11a97ad67a5ce658e99118b450ae8a4cd
melohan <melodie.ohan@cpnv.ch>
2022-01-27 08:47:33 +0100

    Move opinions content verification in opinion component

--- resources/views/components/opinion/opinion.blade.php
+++ resources/views/components/opinion/opinion.blade.php
@@ -1,58 +1,60 @@
-<article class="w-4/5 shadow text-gray-800 mb-10 border">
-    <div class="h-10 bg-gray-100 p-2 flex space-x-80 px-4 ">
-        <div class="w-4/6"><h1 class="font-bold text-lg ">
-                Opinions
-            </h1>
+@if(count($practice->opinions)>0)
+    <article class="w-4/5 shadow text-gray-800 mb-10 border">
+        <div class="h-10 bg-gray-100 p-2 flex space-x-80 px-4 ">
+            <div class="w-4/6"><h1 class="font-bold text-lg ">
+                    Opinions
+                </h1>
+            </div>
         </div>
-    </div>
-    @foreach($practice->opinions as $opinion)
-        <div class="bg-gray-50 p-4 divide-y">
-            <table class="table-auto mb-5">
-                <tr>
-                    <td class="font-bold p-1">Auteur</td>
-                    <td class="p-1">
-                        <a class="underline"
-                           href="{{route('profile' ,  ['id' => $opinion->user->id])}}">{{$opinion->user->name}}</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="font-bold p-1">Date de création</td>
-                    <td class="p-1">{{\Carbon\Carbon::parse($opinion->created_at)->formatLocalized('%d %B %Y')}}</td>
-                </tr>
-                <tr>
-                    <td class="font-bold p-1">Date de modification</td>
-                    <td class="p-1">{{\Carbon\Carbon::parse($opinion->updated_at)->formatLocalized('%d %B %Y')}}</td>
-                </tr>
-                <tr>
-                    <td class="font-bold p-1">Description</td>
-                    <td><p class="text-justify">{{$opinion->description}}</p></td>
-                </tr>
-                <tr>
-                    <td class="font-bold p-1">Votes</td>
-                    <td class="p-1">
-                        <i class="fas fa-thumbs-up text-gray-700"></i> {{$opinion->upvotes()}}
-                        <i class="fas fa-thumbs-down text-gray-700"></i> {{$opinion->downvotes()}}
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2" class="font-normal p-1">
+        @foreach($practice->opinions as $opinion)
+            <div class="bg-gray-50 p-4 divide-y">
+                <table class="table-auto mb-5">
+                    <tr>
+                        <td class="font-bold p-1">Auteur</td>
+                        <td class="p-1">
+                            <a class="underline"
+                               href="{{route('profile' ,  ['id' => $opinion->user->id])}}">{{$opinion->user->name}}</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="font-bold p-1">Date de création</td>
+                        <td class="p-1">{{\Carbon\Carbon::parse($opinion->created_at)->formatLocalized('%d %B %Y')}}</td>
+                    </tr>
+                    <tr>
+                        <td class="font-bold p-1">Date de modification</td>
+                        <td class="p-1">{{\Carbon\Carbon::parse($opinion->updated_at)->formatLocalized('%d %B %Y')}}</td>
+                    </tr>
+                    <tr>
+                        <td class="font-bold p-1">Description</td>
+                        <td><p class="text-justify">{{$opinion->description}}</p></td>
+                    </tr>
+                    <tr>
+                        <td class="font-bold p-1">Votes</td>
+                        <td class="p-1">
+                            <i class="fas fa-thumbs-up text-gray-700"></i> {{$opinion->upvotes()}}
+                            <i class="fas fa-thumbs-down text-gray-700"></i> {{$opinion->downvotes()}}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="font-normal p-1">
 
-                        <div x-data="{ open: false }">
-                            <button class="font-bold" x-on:click="open = ! open" type="button">
-                                Commentaires
-                            </button>
+                            <div x-data="{ open: false }">
+                                <button class="font-bold" x-on:click="open = ! open" type="button">
+                                    Commentaires
+                                </button>
 
-                            <div x-show="open">
-                                @foreach($opinion->comments as $comment)
-                                    <x-opinion.comments :comment="$comment"/>
-                                @endforeach
+                                <div x-show="open">
+                                    @foreach($opinion->comments as $comment)
+                                        <x-opinion.comments :comment="$comment"/>
+                                    @endforeach
                                     <x-opinion.comment-form :opinion="$opinion"/>
+                                </div>
                             </div>
-                        </div>
-                    </td>
-                </tr>
-            </table>
+                        </td>
+                    </tr>
+                </table>
 
-        </div>
-    @endforeach
-</article>
+            </div>
+        @endforeach
+    </article>
+@endif
--- resources/views/practice/details.blade.php
+++ resources/views/practice/details.blade.php
@@ -4,25 +4,21 @@
 
         <x-practice.practice-full :practice="$practice"/>
 
-        @if(!is_null($practice->opinions))
-            <x-opinion.opinion :practice="$practice"/>
-        @endif
+        <x-opinion.opinion :practice="$practice"/>
 
-        @if($practice->isProposed())
-            @can('publish', $practice)
-
+        @can('publish', $practice)
+            @if($practice->isProposed())
                 <form method="POST"
                       action="{{ route('practices.publish', $practice->id) }}"
-                      x-init="displayActions=true"
-                >
+                      x-init="displayActions=true">
                     @csrf
                     <button type="submit"
                             class="mb-8 bg-gray-700 text-white font-semibold py-2 px-4 border border-gray-400 rounded shadow right-0">
                         Publier
                     </button>
                 </form>
-            @endcan
-        @endif
+            @endif
+        @endcan
     @else
         <div class="w-5/6 bg-gray-700 border-l-4 border-gray-400 text-white p-4" role="alert">
             <p class="font-bold text-center">
