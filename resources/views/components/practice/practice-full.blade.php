6335ce631ab96cca6a2da5e254981e0af8ec5b87
melohan <melodie.ohan@cpnv.ch>
2022-01-27 15:33:53 +0100

    Display title form on click on Update button

--- resources/views/components/practice/practice-full.blade.php
+++ resources/views/components/practice/practice-full.blade.php
@@ -1,32 +1,49 @@
-    <!-- Practice -->
-    <article class="w-4/5 shadow text-gray-800 mb-10">
-        <div class="h-10 bg-gray-700 text-white p-2 flex space-x-80 px-4 ">
-            <div class="w-4/6"><h1 class="font-bold text-lg ">
-                    {{$practice->title}}
-                </h1>
+<!-- Practice -->
+<article class="w-4/5 shadow text-gray-800 mb-10">
+    <div class="h-15 bg-gray-700 text-white p-2 flex space-x-80 px-4 ">
+
+        <!-- Title update form -->
+        <div class=" w-4/6">
+            <h1 class="font-bold text-lg ">
+                {{$practice->title}}
+            </h1>
+            <div x-data="{ open: false }">
+                <button
+                    class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 "
+                    x-on:click="open = ! open"
+                    type="button">
+                    Modifier
+                </button>
+                <div x-show="open">
+                    <x-practice.update-title-form :practice="$practice"/>
+                </div>
             </div>
+
         </div>
-        <div class="rounded-b-md bg-gray-50 p-4 ">
-            <table class="table-auto mb-5">
-                <tr>
-                    <td class="font-bold p-1">Domaine</td>
-                    @if(!is_null($practice->domain))
-                        <td class="p-1">{{$practice->domain->name}}</td>
-                    @endif
-                </tr>
-                <tr>
-                    <td class="font-bold p-1">Auteur</td>
-                    <td class="p-1">{{$practice->user->fullname}}</td>
-                </tr>
-                <tr>
-                    <td class="font-bold p-1">Date de création</td>
-                    <td class="p-1">{{\Carbon\Carbon::parse($practice->created_at)->formatLocalized('%d %B %Y')}}</td>
-                </tr>
-                <tr>
-                    <td class="font-bold p-1">Date de modification</td>
-                    <td class="p-1">{{\Carbon\Carbon::parse($practice->updated_at)->formatLocalized('%d %B %Y')}}</td>
-                </tr>
-            </table>
-            <p class="text-justify">{{$practice->description}}</p>
-        </div>
-    </article>
+
+
+    </div>
+    <div class="rounded-b-md bg-gray-50 p-4 ">
+        <table class="table-auto mb-5">
+            <tr>
+                <td class="font-bold p-1">Domaine</td>
+                @if(!is_null($practice->domain))
+                    <td class="p-1">{{$practice->domain->name}}</td>
+                @endif
+            </tr>
+            <tr>
+                <td class="font-bold p-1">Auteur</td>
+                <td class="p-1">{{$practice->user->fullname}}</td>
+            </tr>
+            <tr>
+                <td class="font-bold p-1">Date de création</td>
+                <td class="p-1">{{\Carbon\Carbon::parse($practice->created_at)->formatLocalized('%d %B %Y')}}</td>
+            </tr>
+            <tr>
+                <td class="font-bold p-1">Date de modification</td>
+                <td class="p-1">{{\Carbon\Carbon::parse($practice->updated_at)->formatLocalized('%d %B %Y')}}</td>
+            </tr>
+        </table>
+        <p class="text-justify">{{$practice->description}}</p>
+    </div>
+</article>
--- resources/views/components/practice/update-title-form.blade.php
+++ resources/views/components/practice/update-title-form.blade.php
@@ -1,3 +1,26 @@
 <div>
-    <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
+    @auth
+        <div class="row mt-3">
+            <div class="col-10 row">
+                <form action="{{ route('practice.updateTitle', $practice->id) }}" method="post">
+                    @csrf
+                    <div class="row">
+                        <input id="title"
+                               class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                               id="grid-last-name"
+                               name="title"
+                               type="text"
+                               required
+                               value="">
+
+                        <input type="submit"
+                               value="Soumettre"
+                               class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 inline-flex items-center"
+                        >
+                    </div>
+                </form>
+            </div>
+        </div>
+    @endauth
+
 </div>
