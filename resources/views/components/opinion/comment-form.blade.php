1457a4c0c2b9e672f4e16bedf4b190bdb18ba29b
melohan <melodie.ohan@cpnv.ch>
2022-01-27 10:47:20 +0100

    Hide comment form if user is not logged in

--- resources/views/components/opinion/comment-form.blade.php
+++ resources/views/components/opinion/comment-form.blade.php
@@ -1,17 +1,18 @@
-<div class="row mt-3">
-    <div class="col-2 small text-gray-500 toggling mb-2" data-target="opinions">
-        <div class="bg-warning">
-            Mon opinion:
+@auth
+    <div class="row mt-3">
+        <div class="col-2 small text-gray-500 toggling mb-2" data-target="opinions">
+            <div class="bg-warning">
+                Mon opinion:
+            </div>
+        </div>
+        <div class="col-10 row">
+            <form action="{{ route('opinion.comment', $opinion->id) }}" method="post">
+                @csrf
+                <div class="row">
+                    <textarea id="opinion" name="opinion" class="col-10" required></textarea><br>
+                    <button type="submit" class="btn btn-sm btn-primary col-2">Soumettre</button>
+                </div>
+            </form>
         </div>
     </div>
-    <div class="col-10 row">
-        <form action="{{ route('opinion.comment', $opinion->id) }}" method="post">
-            @csrf
-            <div class="row">
-                <textarea id="opinion" name="opinion" class="col-10" required></textarea><br>
-                <button type="submit" class="btn btn-sm btn-primary col-2">Soumettre</button>
-            </div>
-        </form>
-    </div>
-</div>
-
+@endauth
